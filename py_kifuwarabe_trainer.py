58993357d1e5ecaa851075adf4ea4e4dfac99a62
むずでょ <muzudho1@gmail.com>
2024-09-04 18:59:41 +0900

    DefaultUsiEngine 用意

--- py_kifuwarabe_trainer.py
+++ py_kifuwarabe_trainer.py
@@ -7,8 +7,162 @@ class DefaultUsiEngine():
     """USI エンジン"""
 
 
-    def __init__(self):
-        """初期化"""
+    def __init__(self, engine_file_path):
+        """初期化
+        
+        Parameters
+        ----------
+        engine_file_path : str
+            エンジン名が書かれたテキストファイルへのパス
+        """
+
+        self._engine_file_path = engine_file_path
 
         # 盤
         self._board = cshogi.Board()
+
+
+    def usi_loop(self):
+        """USIループ"""
+
+        while True:
+
+            # 入力
+            cmd = input().split(' ', 1)
+
+            # USIエンジン握手
+            if cmd[0] == 'usi':
+                self.usi()
+
+            # 対局準備
+            elif cmd[0] == 'isready':
+                self.isready()
+
+            # 新しい対局
+            elif cmd[0] == 'usinewgame':
+                self.usinewgame()
+
+            # 局面データ解析
+            elif cmd[0] == 'position':
+                self.position(cmd)
+
+            # 思考開始～最善手返却
+            elif cmd[0] == 'go':
+                self.go()
+
+            # 中断
+            elif cmd[0] == 'stop':
+                self.stop()
+
+            # 対局終了
+            elif cmd[0] == 'gameover':
+                self.gameover(cmd)
+
+            # アプリケーション終了
+            elif cmd[0] == 'quit':
+                break
+
+            # 以下、独自拡張
+
+            # 一手指す
+            # example: ７六歩
+            #       code: do 7g7f
+            elif cmd[0] == 'do':
+                self.do(cmd)
+
+            # 一手戻す
+            #       code: undo
+            elif cmd[0] == 'undo':
+                self.undo()
+
+
+    def usi(self):
+        """USIエンジン握手"""
+
+        # エンジン名は別ファイルから読込。pythonファイルはよく差し替えるのでデータは外に出したい
+        try:
+            with open(self._engine_file_path, 'r', encoding="utf-8") as f:
+                engine_name = f.read().strip()
+
+        except FileNotFoundError as ex:
+            print(f"[usi protocol > usi] '{self._engine_file_path}' file not found.  ex:{ex}")
+            raise
+
+        print(f'id name {engine_name}')
+        print('usiok', flush=True)
+
+
+    def isready(self):
+        """対局準備"""
+        print('readyok', flush=True)
+
+
+    def usinewgame(self):
+        """新しい対局"""
+        print(f"[{datetime.datetime.now()}] usinewgame end", flush=True)
+
+
+    def position(self, cmd):
+        """局面データ解析"""
+        pos = cmd[1].split('moves')
+        sfen_text = pos[0].strip()
+        # 区切りは半角空白１文字とします
+        moves_text = (pos[1].split(' ') if len(pos) > 1 else [])
+        self.position_detail(sfen_text, moves_text)
+
+
+    def position_detail(self, sfen_text, moves_text_as_usi):
+        """局面データ解析"""
+
+        # 平手初期局面に変更
+        if sfen_text == 'startpos':
+            self._board.reset()
+
+        # 指定局面に変更
+        elif sfen_text[:5] == 'sfen ':
+            self._board.set_sfen(sfen_text[5:])
+
+        # 棋譜再生
+        for move_as_usi in moves_text_as_usi:
+            self._board.push_usi(move_as_usi)
+
+
+    def stop(self):
+        """中断"""
+        print('bestmove resign', flush=True)
+
+
+    def gameover(self, cmd):
+        """対局終了"""
+
+        if 2 <= len(cmd):
+            # 負け
+            if cmd[1] == 'lose':
+                print("（＞＿＜）負けた")
+
+            # 勝ち
+            elif cmd[1] == 'win':
+                print("（＾▽＾）勝ったぜ！")
+
+            # 持将棋
+            elif cmd[1] == 'draw':
+                print("（ー＿ー）持将棋か～")
+
+            # その他
+            else:
+                print(f"（・＿・）何だろな？：'{cmd[1]}'")
+
+
+    def do(self, cmd):
+        """一手指す
+        example: ７六歩
+            code: do 7g7f
+        """
+        self._board.push_usi(cmd[1])
+
+
+    def undo(self):
+        """一手戻す
+            code: undo
+        """
+        self._board.pop()
